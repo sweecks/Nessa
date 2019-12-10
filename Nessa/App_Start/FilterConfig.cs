84d6a66eed377c808cdb77c2325df47228816e95
Kristiyan Tanchev <kristiqn.tanchev@gmail.com>
2019-12-10 22:18:18 +0200

    Add https to the project

--- Nessa/App_Start/FilterConfig.cs
+++ Nessa/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace Nessa
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
